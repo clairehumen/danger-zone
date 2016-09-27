5afbd212cec108d064c8c80909d6d6f4b50f084e
clairehumen <humen.claire@gmail.com>
2016-09-27 12:26:32 -0700

    Add files via upload

---
+++ AraayDemo.java
@@ -0,0 +1,16 @@
+public class ArrayDemo {
+
+    
+    public static void main(String[] args)
+    {
+        
+        Integer[] arrayOfIntegers = new Integer[] {1000,2000,3000,4000,5000    };
+
+        for ( Integer i = 0 ; i < arrayOfIntegers.length ; i++ )
+        {
+            System.out.println(arrayOfIntegers[i]);
+        }
+        
+    }
+
+}
---
+++ TimmyTurner.java
@@ -0,0 +1,22 @@
+public class TimmyTurnerArray {
+
+    
+    public static void main(String[] args)
+    {
+        
+        String[] thisIsAStringArray = new String[] {"obtuse" , "rubber goose" , "green moose" , "guava juice" , "giant snake" , "birthday cake" , "large fry" , "chocolate shake" };
+        {
+            System.out.println(thisIsAStringArray[0]);
+       		System.out.println(thisIsAStringArray[1]);
+        	System.out.println(thisIsAStringArray[2]);
+       		System.out.println(thisIsAStringArray[3]);
+        	System.out.println(thisIsAStringArray[4]);
+        	System.out.println(thisIsAStringArray[5]);
+        	System.out.println(thisIsAStringArray[6]);
+          	System.out.println(thisIsAStringArray[7]);
+        }
+      
+        
+    }
+
+}
