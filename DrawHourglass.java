58fdc1affff025f5993e51133279a3f3998ef6e7
Claire Humen <humen.claire@gmail.com>
2016-10-05 20:48:24 -0700

    Add files via upload

---
+++ DrawHourglass.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class DrawHourglass {
+
+    public static void main(String[] args) {
+        DrawLine();
+        DrawTop();
+        DrawBottom();
+        DrawLine();
+    }
+
+   // solid line instructions
+    public static void DrawLine() {
+        System.out.print("+");
+        for (int i = 1; i <= 6; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println("+");
+    }
+
+    // top half of hourglass
+    public static void DrawTop() {
+        for (int line = 1; line <=3; line++) {
+            System.out.print("|");
+            for(int i = 1; i <= (line - 1); i++) {
+                System.out.print(" ");
+
+            }
+            System.out.print("\\");
+            for (int i = 1; i <= (6 - 2 * line); i++) {
+                System.out.print(".");
+        }
+        System.out.print("/");
+            for (int i = 1; i <= (line - 1); i++) {
+           System.out.print(" ");
+
+            }
+        System.out.println("|");
+
+    }
+}
+
+    // bottom half of hourglass
+    public static void DrawBottom() {
+        for (int line = 1; line <= 3; line++) {
+            System.out.print("|");
+            for (int i = 1; i <= (3 - line); i++) {
+                System.out.print(" ");
+
+            }
+            System.out.print("/");
+            for (int i = 1; i <= 2 * (line - 1); i++) {
+                System.out.print(".");
+            }
+            System.out.print("\\");
+            for (int i = 1; i <= (3 - line); i++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
+    }
+}
