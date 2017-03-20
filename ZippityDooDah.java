68b988e924bbc84336cacb8aface5e005b796ee8
nhojster <nhojster@192.168.0.3>
2017-03-19 21:35:35 -0700

    Can you see this

---
+++ ZippityDooDah.java
@@ -0,0 +1,14 @@
+public class ZippityDooDah {
+
+    
+    public static void main(String[] args)
+    {
+        
+    	System.out.println("Hello.");
+    	// Claire, all I need you to do is print your name.
+    	// Further, you need to find out a way to "push" this code to GIT HUB.
+    	// The idea is that you are making a change to this code and putting it where I can get at it.
+    	
+    }
+
+}
