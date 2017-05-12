074a46d742c022530789cb531a34e6effdbb7e6e
clairehumen <humen.claire@gmail.com>
2017-05-11 20:40:38 -0700

    Music stuff

--- HarmonicMinorScale.java
+++ HarmonicMinorScale.java
@@ -2,4 +2,7 @@
  * Created by clairehumen on 3/20/17.
  */
 public class HarmonicMinorScale extends MinorScale {
+    public int[] getSequence() {
+        return new int[] {WHOLE, HALF, WHOLE, WHOLE, HALF, WHOLE, HALF};
+    }
 }
